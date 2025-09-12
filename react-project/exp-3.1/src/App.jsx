52f76992c648050f9127ac252b6a78121f0eccae
nutu-123 <phadtarenutan2004@gmail.com>
2025-09-12 10:45:47 +0530

    commited

---
+++ react-project/exp-3.1/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductCard from './components/ProductCard'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const product1 = { name: "Wireless Mouse", price: 25.99, status: "In Stock" }
+  const product2 = { name: "Keyboard", price: 45.5, status: "Out of Stock" }
+  const product3 = { name: "Monitor", price: 199.99, status: "In Stock" }
+
+  return (
+    <>
+      <h1>Nutan Phadtare</h1>
+      <h2>Product List</h2>
+      <div id="Product-List">
+        <ProductCard name={product1.name} price={product1.price} status={product1.status} />
+        <ProductCard name={product2.name} price={product2.price} status={product2.status} />
+        <ProductCard name={product3.name} price={product3.price} status={product3.status} />
+      </div>
+      
+
+    </>
+
+  )
+}
+
+export default App
